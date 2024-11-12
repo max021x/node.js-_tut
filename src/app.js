5fb8e0d203df70b4d9bbe32a1279b490c85d64fd
Max <Max.021persia@gmail.com>
2024-11-12 18:21:26 +0330

    added files

---
+++ src/app.js
@@ -0,0 +1,37 @@
+// basic of node 
+/*
+const http = require('http') ;
+// req , res 
+const server = http.createServer((req , res) =>{
+  res.statusCode = 200 ;
+  // what kinde of informations we are giving back 
+  // res.setHeader('Content-Type' , 'text/plain');
+  res.setHeader('Content-Type' , 'text/html');
+  // what the user get back 
+  res.end('<h1>welcome to my fucking page</h1>') ;
+}); 
+// 
+server.listen(3000 , '127.0.0.1' , ()=>{
+  console.log("server running ... ");
+}) ; */
+
+
+// how to create peoject 
+// npm init -y 
+// npm start
+
+
+// npm depencies 
+// how to install 
+// npm install uuid  // a package to generate random identifiers
+// npmjs has all source code init 
+
+// creating a unic indentifire
+// const {v4 : uuid4} = require('uuid') ;
+// console.log(uuid4());
+
+
+// git 
+
+
+
